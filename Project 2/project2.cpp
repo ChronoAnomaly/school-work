bd0c1d99822041947971aba887cc682aa1dff4b2
ChronoAnomaly <chronoanomaly@gmail.com>
2014-10-03 12:49:43 -0400

    command interpreter done

--- Project 2/project2.cpp
+++ Project 2/project2.cpp
@@ -17,7 +17,75 @@ int main()
 		// convert to uppercase
 		cmd = toupper ( cmd );
 
-		// insert code to process command
+		if(cmd == 'N'){
+		
+			bt.next();
+
+		} else if(cmd == 'P') {
+		
+			bt.prev();
+
+		} else if(cmd == 'V') {
+
+			bt.value();
+
+		} else if(cmd == 'D') {
+
+			bt.display();
+
+		} else if(cmd == 'F') {
+
+			bt.isFirst();
+
+		} else if(cmd == 'L') {
+
+			bt.isLast();
+
+		} else if(cmd == 'H') {
+
+			bt.isHead();
+
+		} else if(cmd == 'E') {
+
+			bt.isEmpty();
+
+		} else if(cmd == 'S') {
+
+			int value;
+			cin >> value;
+
+			bt.find( value);
+
+		} else if(cmd == '<') {
+
+			bt.findMin();
+
+		} else if(cmd == '>') {
+
+			bt.findMax();
+
+		} else if(cmd == '+') {
+
+			int value;
+			cin >> value;
+
+			bt.insert( value);
+		} else if(cmd == '-') {
+
+			int value;
+			cin >> value;
+
+			bt.remove( value);
+
+		} else if(cmd == 'M') {
+
+			bt.removeMin();
+
+		} else if(cmd == 'X') {
+
+			bt.removeMax();
+
+		} 
 	}
 	while( cmd != 'Q');
 }
